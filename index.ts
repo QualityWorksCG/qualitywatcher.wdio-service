b27db489d5e41fcaebfac13eb10448b54a04e9ca
Dimitri Harding <vaughnharding@hotmail.com>
2022-04-21 15:12:56 -0500

    refactor: update service to log help full messages

--- index.ts
+++ index.ts
@@ -1,117 +1,197 @@
-import fs from 'fs'
-const fetch = require('node-fetch')
-const { SevereServiceError } = require('webdriverio')
+import fs from "fs";
+const fetch = require("node-fetch");
+const { SevereServiceError } = require("webdriverio");
+const colors = require("ansi-colors");
 
 interface userOptions {
-    email: string,
-    apiKey: string,
-    testRunName: string,
-    description: string,
-    projectId: Number,
-    includeAllCases: boolean,
+  email: string;
+  apiKey: string;
+  testRunName: string;
+  description: string;
+  projectId: Number;
+  includeAllCases: boolean;
 }
 
 interface QWWDIOReporterInterface {
-    options: userOptions;
+  options: userOptions;
 }
 
 export default class QWWDIOService implements QWWDIOReporterInterface {
-    private results = []
-    private dir = './QualityWatcher'
+  private results = [];
+  private dir = "./QualityWatcher";
 
-    options: {
-        email: '',
-        apiKey: '',
-        testRunName: '',
-        description: '',
-        projectId: 10,
-        includeAllCases: false,
-    }
+  options: {
+    email: "";
+    apiKey: "";
+    testRunName: "";
+    description: "";
+    projectId: 10;
+    includeAllCases: false;
+  };
 
-    constructor(serviceOptions, capabilities, config) {
-        this.options = serviceOptions
-        validateOptions(serviceOptions)
-    }
+  constructor(serviceOptions) {
+    this.options = serviceOptions;
+    validateOptions(serviceOptions);
+  }
 
-    async onPrepare() {
-        if (fs.existsSync(this.dir)) {
-            try {
-                await fs.rmSync(this.dir, { recursive: true })
-            } catch (err) {
-                console.error(err)
-            }
-        }
-    }
+  async onPrepare() {
+    await removeReportFolder(this.dir);
+  }
 
-    async onComplete() {
-        let files = fs.readdirSync(this.dir)
-        let suites = []
+  async onComplete() {
+    let files = fs.readdirSync(this.dir);
+    let suites = [];
 
-        await files.forEach(async (fileName) => {
+    await files.forEach(async (fileName) => {
+      suites.push(getSuiteId(fileName));
+      try {
+        const data = JSON.parse(
+          await fs.readFileSync(this.dir + "/" + fileName, "utf8")
+        );
 
-            suites.push(getSuiteId(fileName))
-            try {
-                const data = JSON.parse(await fs.readFileSync(this.dir + '/' + fileName, 'utf8'))
+        data.forEach(async (test) => {
+          await this.results.push(test);
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    });
 
-                data.forEach(async (test) => {
-                    await this.results.push(test)
-                })
-            } catch (err) {
-                console.error(err)
-            }
+    try {
+      fs.writeFileSync(
+        this.dir + "/results.json",
+        JSON.stringify(this.results)
+      );
+
+      console.log(colors.bold.green(`\n"${"QualityWatcher"}" Reporter`));
+      console.log(
+        "------------------------------------------------------------------"
+      );
+
+      if (this.results.length === 0) {
+        console.log(
+          "[QualityWatcher] ",
+          colors.red(
+            "No tests were run. Please check your test files and ensure that you have suite and test case IDs."
+          )
+        );
+        return;
+      }
+
+      let requestBody = {
+        testRunName: this.options.testRunName,
+        description: this.options.description,
+        include_all_cases: this.options.includeAllCases,
+        projectId: parseInt(`${this.options.projectId}`),
+        suites: suites,
+        results: this.results,
+      };
+
+      await postData(
+        "https://1k0og4tfve.execute-api.us-east-1.amazonaws.com/prod/nimble/v1/test-runner/add-automated-test-execution",
+        requestBody,
+        this.options.apiKey
+      )
+        .then(async (response) => {
+          const data = await response.json();
+          if (response.status === 200) {
+            return data;
+          }
+          throw { response, data };
         })
+        .then((responseData) => {
+          console.log(
+            `[QualityWatcher] Test run "${responseData.title}" has been added to QualityWatcher`,
+            `\n[QualityWatcher] Results published: `,
+            colors.green(
+              `https://app.qualitywatcher.com/${responseData.projectId}/test-runner/${responseData.id}`
+            )
+          );
+        })
+        .catch(({ response, data }) => {
+          colors.red(`[QualityWatcher] There was an error publishing results.`);
+          if (response.status === 500) {
+            console.log(
+              `[QualityWatcher] ${colors.red(
+                `Ensure that your API key is correct.`
+              )}`
+            );
+          } else {
+            console.log(
+              "[QualityWatcher] ",
+              colors.red(
+                `Bad Request(${data.error.code}): ${data.error.message}`
+              )
+            );
+          }
+        });
 
-        try {
-            fs.writeFileSync(this.dir + '/results.json', JSON.stringify(this.results))
-
-            let requestBody = {
-                "testRunName": this.options.testRunName,
-                "description": this.options.description,
-                "include_all_cases": this.options.includeAllCases,
-                "projectId": parseInt(this.options.projectId),
-                "suites": suites,
-                "results": this.results
-            }
-
-            await postData('https://d6qp7e7hkb.execute-api.us-east-1.amazonaws.com/dev/nimble/v1/test-runner/add-automated-test-execution', requestBody, this.options.apiKey)
-                .then(data => {
-                    console.log('QualityWatcher Results')
-                    console.log(data)
-                })
-
-        } catch (err) {
-            console.error(err)
-        }
+      await removeReportFolder(this.dir);
+    } catch (error) {
+      console.log(
+        colors.red(
+          `[QualityWatcher] There was an error publishing results: ${error}`
+        )
+      );
     }
+  }
 }
 
 function getSuiteId(fileName) {
-    let suiteRegex = /\d+/ig
-    return parseInt(fileName.match(suiteRegex)[0])
+  let suiteRegex = /\d+/gi;
+  return parseInt(fileName.match(suiteRegex)[0]);
 }
 
 function validateOptions(options) {
-    if (!options?.email) { throw new SevereServiceError("Missing property for QualityWatcherService: 'email' [string]") }
-    if (!options?.apiKey) { throw new SevereServiceError("Missing property for QualityWatcherService: 'apiKey' [string]") }
-    if (!options?.testRunName) { throw new SevereServiceError("Missing property for QualityWatcherService: 'testRunName' [string]") }
-    if (!options?.description) { throw new SevereServiceError("Missing property for QualityWatcherService: 'description' [string]") }
-    if (!options?.projectId) { throw new SevereServiceError("Missing property for QualityWatcherService: 'projectId' [number]") }
-    if (!options?.includeAllCases) { throw new SevereServiceError("Missing property for QualityWatcherService: 'includeAllCases' [boolean]") }
+  if (!options?.email) {
+    throw new SevereServiceError(
+      "Missing property for QualityWatcherService: 'email' [string]"
+    );
+  }
+  if (!options?.apiKey) {
+    throw new SevereServiceError(
+      "Missing property for QualityWatcherService: 'apiKey' [string]"
+    );
+  }
+  if (!options?.testRunName) {
+    throw new SevereServiceError(
+      "Missing property for QualityWatcherService: 'testRunName' [string]"
+    );
+  }
+  if (!options?.description) {
+    throw new SevereServiceError(
+      "Missing property for QualityWatcherService: 'description' [string]"
+    );
+  }
+  if (!options?.projectId) {
+    throw new SevereServiceError(
+      "Missing property for QualityWatcherService: 'projectId' [number]"
+    );
+  }
+  if (!options?.includeAllCases) {
+    throw new SevereServiceError(
+      "Missing property for QualityWatcherService: 'includeAllCases' [boolean]"
+    );
+  }
 }
 
 async function postData(url, data, apiKey) {
-    const response = await fetch(url, {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${apiKey}`
-        },
-        redirect: 'follow',
-        referrerPolicy: 'no-referrer',
-        body: JSON.stringify(data)
-    })
-    return response.json()
+  return await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify(data),
+  });
+}
+
+async function removeReportFolder(directory) {
+  if (fs.existsSync(directory)) {
+    try {
+      await fs.rmSync(directory, { recursive: true });
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
